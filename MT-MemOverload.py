461396ab9dddcf771cd71b9070360cacb86be746
MillenniumWare <89882579+MillenniumWare@users.noreply.github.com>
2021-09-01 20:56:06 -0600

    Add files via upload

---
+++ MT-MemOverload.py
@@ -0,0 +1,18 @@
+import os
+import multiprocessing
+from multiprocessing import Process
+
+
+def listfill():
+    filllist = []
+    for i in range(9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999):
+        filllist.append("0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000")
+
+
+
+cpucount = os.cpu_count()
+cpucount = int(cpucount)
+if __name__ == '__main__':
+    p = Process(target=listfill)
+    p.start()
+    p.join()
